91b677a76d2924f8a328ecd29a1905e348be35f6
Issao Hanaoka Junior <issaojr@gmail.com>
2023-12-29 03:25:05 -0300

    Olá mundo! Este é o primeiro commit do aplicativo Conversor WEBP, publicado sob licença Apache 2.0. Já está basicamente funcional, mas ainda precisa de trabalho. O layout precisa ser melhorado e novas funcionalidades adicionadas, sobretudo com relação a configurações da qualidade de conversão.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Conversor de Imagens WebP
+
+Este é um aplicativo de desktop simples, desenvolvido em Python, que permite converter imagens nos formatos PNG, JPEG e TIFF para o formato WebP.
+
+## Funcionalidades
+
+- Seleção de vários arquivos ou um diretório inteiro para conversão.
+- Opção para remover arquivos da lista de entrada.
+- Evita a sobrescrita de arquivos existentes adicionando um sufixo ao nome do arquivo.
+- Interface amigável com espaçamento adequado entre os itens e alinhamento dos botões à direita.
+- Menu com opções 'Sobre', 'Licença de uso' e 'Ajuda'.
+- É importante ressaltar que este aplicativo não apaga ou modifica os arquivos originais, mas apenas cria novos arquivos no formato WEBP, e caso já existam arquivos WEBP com o mesmo nome, novos arquivos serão criados com sufixo nomedoarquivo(1).webp. Isto garante que nenhum arquivo será sobrescrito ou apagado por equívoco.
+
+## Como usar
+
+1. Clique em 'Selecionar Diretório' para escolher um diretório inteiro para conversão, ou clique em 'Selecionar arquivo(s)' para escolher arquivos individuais.
+2. Se quiser remover algum arquivo da lista de entrada, selecione o arquivo e clique em 'Remover selecionado'.
+3. Escolha o diretório de saída clicando em 'Selecionar Diretório' ao lado do campo 'Selecione o diretório de saída:'.
+4. Clique em 'Converter' para iniciar a conversão dos arquivos. Você será notificado quando a conversão for concluída.
+
+## Dependências
+
+- Python 3
+- PyQt5
+- cwebp (libwebp-1.3.2-windows-x64 (https://developers.google.com/speed/webp/docs/precompiled?hl=pt-br - incluído no diretório do projeto)
+
+## Desenvolvedor
+
+Desenvolvido por: Issao Hanaoka Junior
+E-mail: issaojr.dev@gmail.com
+Versão: 0.5.0
+Data: 29/12/2023
---
+++ conversorwebp.py
@@ -0,0 +1,190 @@
+# Copyright 2023 Issao Hanaoka Junior
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###
+### Conversor de arquivos de imagem para WEBP
+### Utiliza a biblioteca libwebp-1.3.2-windows-x64 (https://developers.google.com/speed/webp/docs/precompiled?hl=pt-br)
+### Publicado inicialmente em 29/12/2023
+###
+
+# Importando as bibliotecas necessárias
+import os
+import subprocess
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, QLabel, QLineEdit, QMenu, QAction, QMessageBox, QListWidget
+from PyQt5.QtCore import Qt
+
+# Função para converter imagens para o formato webp
+def convert_to_webp(input_file, output_file):
+    base_output_file = output_file
+    i = 1
+    # Verifica se o arquivo de saída já existe e adiciona um sufixo se necessário
+    while os.path.exists(output_file):
+        output_file = f"{base_output_file.rsplit('.', 1)[0]}({i}).webp"
+        i += 1
+    try:
+        subprocess.run(["./webp/bin/cwebp.exe", "-q", "80", input_file, "-o", output_file], check=True)
+    except subprocess.CalledProcessError as e:
+        return f"A conversão falhou com o seguinte erro: {e.stderr.decode('utf-8')}"
+    return "Conversão concluída com sucesso!"
+
+# Função para selecionar arquivos de entrada
+def select_input_files():
+    files = QFileDialog.getOpenFileNames()[0]
+    if not files:
+        return
+    directory = os.path.dirname(files[0])
+    for file in files:
+        if os.path.dirname(file) != directory:
+            QMessageBox.critical(None, "Erro", "Todos os arquivos devem estar no mesmo diretório.")
+            return
+    input_files.addItems(files)
+    convert_button.setEnabled(True)  # Habilita o botão quando um arquivo é selecionado
+
+# Função para selecionar um diretório de entrada
+def select_input_directory():
+    directory = QFileDialog.getExistingDirectory()
+    if directory:
+        input_files.addItem(directory)
+        convert_button.setEnabled(True)  # Habilita o botão quando um arquivo é selecionado
+
+# Função para remover um arquivo de entrada
+def remove_input_file():
+    for item in input_files.selectedItems():
+        input_files.takeItem(input_files.row(item))
+        if input_files.count() == 0:
+            convert_button.setEnabled(False)
+
+# Função para selecionar o diretório de saída
+def select_output_dir():
+    output_dir.setText(QFileDialog.getExistingDirectory())
+
+# Função para converter os arquivos de entrada
+def convert():
+    input_files_value = [input_files.item(i).text() for i in range(input_files.count())]
+    output_dir_value = output_dir.text()
+
+    for input_path_value in input_files_value:
+        if os.path.isdir(input_path_value):
+            for filename in os.listdir(input_path_value):
+                if filename.endswith('.png') or filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.tiff'):
+                    input_file = os.path.join(input_path_value, filename)
+                    output_file = os.path.join(output_dir_value, os.path.splitext(filename)[0] + '.webp')
+                    message = convert_to_webp(input_file, output_file)
+                    if "Conversão concluída com sucesso!" in message:
+                        QMessageBox.information(None, "Conversão concluída com sucesso!", f'Os arquivos convertidos foram salvos em {output_dir_value}')
+                    else:
+                        QMessageBox.critical(None, "Erro", message)
+        elif os.path.isfile(input_path_value):
+            if input_path_value.endswith('.png') or input_path_value.endswith('.jpg') or input_path_value.endswith('.jpeg') or input_path_value.endswith('.tiff'):
+                filename = os.path.basename(input_path_value)
+                output_file = os.path.join(output_dir_value, os.path.splitext(filename)[0] + '.webp')
+                message = convert_to_webp(input_file, output_file)
+                if "Conversão concluída com sucesso!" in message:
+                    QMessageBox.information(None, "Conversão concluída com sucesso!", f'Os arquivos convertidos foram salvos em {output_dir_value}')
+                else:
+                    QMessageBox.critical(None, "Erro", message)
+
+# Função para exibir informações sobre o desenvolvedor
+def show_about():
+    QMessageBox.information(None, "Informações sobre o desenvolvedor", 'Desenvolvido por: Issao Hanaoka Junior\nE-mail: issaojr.dev@gmail.com\nVersão: 0.5.0\nData: 28/12/2023')
+
+# Função para exibir a licença de uso
+def show_license():
+    with open('LICENSE.txt', 'r', encoding='utf-8') as file:
+        license_text = file.read()
+    QMessageBox.information(None, "Licença de uso", license_text)
+
+# Função para exibir a ajuda
+def show_help():
+    with open('README.md', 'r', encoding='utf-8') as file:
+        ajuda_text = file.read()
+    QMessageBox.information(None, "Ajuda", ajuda_text)
+    
+
+
+# Criação da aplicação e da janela principal
+app = QApplication([])
+window = QMainWindow()
+central_widget = QWidget()
+layout = QVBoxLayout()
+title = 'Conversor de Imagens para WEBP'
+
+# Criação dos botões e campos de texto
+select_directory_button = QPushButton('Selecionar Diretório')
+select_directory_button.clicked.connect(select_input_directory)
+
+input_files_label = QLabel('Selecione os diretórios ou arquivos para converter:')
+input_files = QListWidget()
+input_files_button = QPushButton('Selecionar arquivo(s)')
+input_files_button.clicked.connect(select_input_files)
+
+remove_button = QPushButton('Remover selecionado')
+remove_button.clicked.connect(remove_input_file)
+
+output_dir_label = QLabel('Selecione o diretório para os arquivos WEBP criados:')
+output_dir = QLineEdit()
+output_dir_button = QPushButton('Selecionar Diretório')
+output_dir_button.clicked.connect(select_output_dir)
+
+convert_button = QPushButton('Converter')
+convert_button.clicked.connect(convert)
+convert_button.setEnabled(False)  # Desabilita o botão inicialmente
+
+# Criação do botão 'Sair'
+exit_button = QPushButton('Sair')
+exit_button.clicked.connect(app.quit)  # Conecta o sinal 'clicked' do botão à função 'quit'
+
+# Adicionando os widgets ao layout
+layout.addWidget(input_files_label)
+layout.addWidget(input_files)
+layout.addWidget(remove_button)
+layout.addWidget(select_directory_button)
+layout.addWidget(input_files_button)
+layout.addWidget(output_dir_label)
+layout.addWidget(output_dir)
+layout.addWidget(output_dir_button)
+layout.addWidget(convert_button)
+layout.addWidget(exit_button)
+
+# Configuração do widget central e do layout
+central_widget.setLayout(layout)
+window.setCentralWidget(central_widget)
+window.setWindowTitle(title)
+
+
+# Criação do menu
+menu = window.menuBar()
+about_menu = QMenu('Sobre', menu)
+about_action = QAction('Informações sobre o desenvolvedor', about_menu)
+about_action.triggered.connect(show_about)
+about_menu.addAction(about_action)
+menu.addMenu(about_menu)
+
+license_menu = QMenu('Licença de uso', menu)
+license_action = QAction('Ler a licença de uso', license_menu)
+license_action.triggered.connect(show_license)
+license_menu.addAction(license_action)
+menu.addMenu(license_menu)
+
+help_menu = QMenu('Ajuda', menu)
+help_action = QAction('Instruções de uso', help_menu)
+help_action.triggered.connect(show_help)
+help_menu.addAction(help_action)
+menu.addMenu(help_menu)
+
+# Exibição da janela
+window.show()
+
+# Execução da aplicação
+app.exec_()
